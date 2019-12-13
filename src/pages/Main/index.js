55a10dd5700ccea4396c86d5dca86866f061fb1d
souza-gsdev <gabrielsouza.gsdev@gmail.com>
2019-12-13 12:59:31 -0300

    configuração basica do eslint e prettier, e criação de rotas

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from './styles';
+// import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Title error={false}>
+      Main
+      <small>Menor</small>
+    </Title>
+  );
+}
